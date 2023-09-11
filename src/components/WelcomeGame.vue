34a96b2297aa500d30cf60b53e68f63c40cdc375
mariovaldivia <mva.guitar@gmail.com>
2023-09-11 18:28:49 -0300

    Improve welcome validation

--- src/components/WelcomeGame.vue
+++ src/components/WelcomeGame.vue
@@ -3,13 +3,22 @@
     <section class="h-100 p-5 z-100 bg-body-tertiary border rounded-3 welcome-overlay">
 
       <h1>Memory Game</h1>
+
       <div class="row">
         <form action="#">
           <div class="mb-3">
             <label for="name" class="form-label">Your name</label>
-            <input type="text" class="form-control form-control-lg" id="name" ref="name" placeholder="Your name" required>
+            <div class="input-group has-validation">
+              <input type="text" class="form-control form-control-lg" :class="{ 'is-invalid': nameInvalid }" id="name"
+                ref="name" required>
+              <div class="invalid-feedback">
+                Please enter your name.
+              </div>
+            </div>
 
-            <button type="button" class="btn btn-success mt-2" @click="setName">Start</button>
+            <button type="button" class="btn btn-success mt-2" @click="setName">
+              Start
+            </button>
 
           </div>
 
@@ -17,7 +26,6 @@
       </div>
     </section>
   </modal>
-
 </template>
   
 <script lang="ts">
@@ -32,20 +40,25 @@ export default {
   },
   setup(props, { emit }) {
     const name = ref(null);
+    const nameInvalid = ref(false);
     const userStore = useUserStore();
 
     const setName = () => {
       const nameValue = name.value?.value
       if (nameValue != '') {
+        nameInvalid.value = false
         console.log("starting game", nameValue)
         userStore.setName(nameValue);
         emit('startGame')
 
+      } else {
+        nameInvalid.value = true
       }
 
     }
     return {
       name,
+      nameInvalid,
       setName
     }
   }
